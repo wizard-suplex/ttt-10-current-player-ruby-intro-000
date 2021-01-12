b9aa3d44ca6b00dc35aefba952fea6a782f570ee
Alan Cooke <atc4@rice.edu>
2021-01-12 20:07:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
 def turn_count(board)
+  turn_number
   turn_number = 0
   board.each do |move|
     if move == "X" || move == "O"
       turn_number += 1
     end
   end
-  turn_number
 end
 
 def current_player(board)
