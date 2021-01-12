a55242056543a8fa81a61bcd0b866ca10177d21b
Alan Cooke <atc4@rice.edu>
2021-01-12 20:02:17 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,6 +9,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  current_player = turn_count(board) % 2 == 0 ? "X" : "O"
-  puts "The current player is #{current_player}"
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
